346be7a842cdb0b8c431314727d83e96d8e6990d
roboli <robertooliveros@mac.com>
2019-06-04 19:33:10 -0600

    Map alpha chars to numbers

--- src/qoes/number.cljs
+++ src/qoes/number.cljs
@@ -14,18 +14,36 @@
    [:> mui/IconButton {:on-click on-click}
     [:> mui-icons/Backspace]]])
 
+(defn map-char [char]
+  (cond
+    (re-matches #"(?i)[abc]" char) "2"
+    (re-matches #"(?i)[def]" char) "3"
+    (re-matches #"(?i)[ghi]" char) "4"
+    (re-matches #"(?i)[jkl]" char) "5"
+    (re-matches #"(?i)[mno]" char) "6"
+    (re-matches #"(?i)[pqrs]" char) "7"
+    (re-matches #"(?i)[tuv]" char) "8"
+    (re-matches #"(?i)[wxyz]" char) "9"
+    (re-matches #"\+" char) "0"
+    :else nil))
+
+(defn on-change [update-num]
+  (fn [val]
+    (let [num (-> val .-target .-value)]
+      (cond
+        (= (count num) 0) (update-num "")
+        (re-matches #"\d+" num) (update-num num)
+        (map-char (last num)) (update-num (str
+                                           (apply str (drop-last num))
+                                           (map-char (last num))))))))
+
 (defn input [{:keys [classes value update-num rm-num] :as props}]
   [:> mui/Input {:class (.-input classes)
                  :inputProps {:style {:textAlign "center"}}
                  :fullWidth true
                  :autoFocus true
                  :value value
-                 :onChange (fn [str]
-                             (let [num (-> str .-target .-value)]
-                               (if (= (count num) 0)
-                                 (update-num "")
-                                 (if (re-matches #"\d+" num)
-                                   (update-num num)))))
+                 :onChange (on-change update-num)
                  :disableUnderline true
                  :endAdornment (r/create-element (r/reactify-component rm-number) #js{:on-click rm-num})}])
 
